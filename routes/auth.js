c3a1e4d91d3b28bc1e13bac9c72df9bafb3e599c
NofarBaron <nofarbaron28@gmail.com>
2023-05-27 11:00:09 +0300

    first changes

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ router.post("/Register", async (req, res, next) => {
       country: req.body.country,
       password: req.body.password,
       email: req.body.email,
-      profilePic: req.body.profilePic
+      // profilePic: req.body.profilePic
     }
     let users = [];
     users = await DButils.execQuery("SELECT username from users");
@@ -30,11 +30,12 @@ router.post("/Register", async (req, res, next) => {
       parseInt(process.env.bcrypt_saltRounds)
     );
     await DButils.execQuery(
-      `INSERT INTO users VALUES ('${user_details.username}', '${user_details.firstname}', '${user_details.lastname}',
+      `INSERT INTO mydb.users (username, firstname, lastname, country, password, email) VALUES ('${user_details.username}', '${user_details.firstname}', '${user_details.lastname}',
       '${user_details.country}', '${hash_password}', '${user_details.email}')`
     );
     res.status(201).send({ message: "user created", success: true });
   } catch (error) {
+    console.log("here the problem")
     next(error);
   }
 });
@@ -44,21 +45,22 @@ router.post("/Login", async (req, res, next) => {
     // check that username exists
     const users = await DButils.execQuery("SELECT username FROM users");
     if (!users.find((x) => x.username === req.body.username))
-      throw { status: 401, message: "Username or Password incorrect" };
+      throw { status: 401, message: "Username2 or Password incorrect" };
 
     // check that the password is correct
     const user = (
       await DButils.execQuery(
-        `SELECT * FROM users WHERE username = '${req.body.username}'`
+        `SELECT * FROM mydb.users WHERE username = '${req.body.username}'`
       )
     )[0];
-
+    // if (!(req.body.password==user.password)){
     if (!bcrypt.compareSync(req.body.password, user.password)) {
-      throw { status: 401, message: "Username or Password incorrect" };
+      throw { status: 401, message: "Username3 or Password incorrect" + req.body.password +user.password };
     }
 
     // Set cookie
-    req.session.user_id = user.user_id;
+    req.session.username = user.username;
+    console.log("user id is: " + req.session.username);
 
 
     // return cookie
